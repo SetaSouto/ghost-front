21ddd2f497295905a5bde393f2d3fe79be21289a
Fabián Souto <fab.souto@gmail.com>
2020-04-19 20:15:54 -0400

    Provide default API_HOST and API_KEY in the api client.
    This is not dangerouse because any way this data must be in the front to make api calls.

--- api/client.js
+++ api/client.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 if (process.env.NODE_ENV === 'test') { require('dotenv').config() }
 
-if (!process.env.API_HOST) { throw new Error('Please provide the API_HOST env variable.') }
-if (!process.env.API_KEY) { throw new Error('Please provide the API_KEY env variable.') }
+const client = axios.create({
+  baseURL: `${process.env.API_HOST || 'https://api.blog.fabiansouto.com'}/ghost/api/v3/content`
+})
 
-const client = axios.create({ baseURL: `${process.env.API_HOST}/ghost/api/v3/content` })
 client.interceptors.request.use((config) => {
-  config.params.key = process.env.API_KEY
+  config.params.key = process.env.API_KEY || 'e549f81ba651f72b68a95d0971'
   return config
 })
 
